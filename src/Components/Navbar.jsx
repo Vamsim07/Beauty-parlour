a66086124425853aff6e20d93b54ed73c0bf4419
Vamsi-009 <vamsimamidi197@gmail.com>
2025-10-24 22:46:46 +0530

    navigation settings

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,28 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
 
+    return(<>
+    <div className="container">
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="navbar">
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="about" className="nav-link">About</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="works" className="nav-link">Works</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/contact" className="nav-link">Contact</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    </>);
 }
 export default Navbar;
