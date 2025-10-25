b23d015192b568cf68645d8ce0be0f5f987bb778
Vamsi-009 <vamsimamidi197@gmail.com>
2025-10-26 00:28:43 +0530

    Adding SMedia

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,33 @@
+import parlourLogo from "../assets/Varshini_Beauty_Logo.png"
 function Home() {
     return(<>
-    <h1>Home Page</h1>
+    <div className="container">
+        <div className="row">
+            <div className="col-md-6">
+                <img src={parlourLogo} alt="logo" height="370px" width="370px"/>
+            </div>
+            <div className="col-md-6">
+                <h1 className="heading text-center fw-semibold mt-5">Welcome To <span className="text-danger">Varshini</span> Beauty Parlour</h1>
+                <h3 className="text-center fst-italic lead m-3">Your beauty, our artistry 💖</h3>
+                <p className="fst-italic fw-bold text-center">Where confidence begins and beauty blooms.<br/> We blend creativity and care to create looks that mirror your inner glow.<br /> Discover a space of calm, color, and charm — because you deserve to feel radiant, always. 🌷</p>
+
+                <div class="d-flex justify-content-center gap-3">
+                    <a href="https://instagram.com/" target="_blank" class="text-decoration-none text-info fs-4">
+                    <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="https://facebook.com/" target="_blank" class="text-decoration-none text-info fs-4">
+                    <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="https://wa.me/919876543210" target="_blank" class="text-decoration-none text-info fs-4">
+                    <i class="fab fa-whatsapp"></i>
+                    </a>
+                    <a href="https://youtube.com/" target="_blank" class="text-decoration-none text-info fs-4">
+                    <i class="fab fa-youtube"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
     </>)
 }
 export default Home; 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -22,34 +22,34 @@ function Navbar() {
     <div className="container">
         <nav className={`navbar navbar-expand-lg ${darkMode ? "navbar-dark bg-dark" : "navbar-light bg-light"}`}>
             <div className="navbar-brand">
-                <img src={darkMode ? lightLogo : darkLogo} alt="logo" height="250px" width="250px" />
+                <img src={darkMode ? lightLogo : darkLogo} alt="logo" height="200px" width="200px" />
             </div>
             <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#hamberger" type="button" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
              <i class="fa-solid fa-bars"></i>  
             </button>
             <div className="collapse navbar-collapse" id="hamberger">
-                <ul className="navbar-nav ms-auto fw-bold">
+                <ul className="navbar-nav ms-auto fw-bold fs-4 fst-italic">
                     <li className="nav-item">
-                        <Link to="/" className="nav-link">Beauty Lounge</Link>
+                        <Link to="/" className="nav-link"><span className="text-danger">B</span>eauty Lounge</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="about" className="nav-link">Our Stoty</Link>
+                        <Link to="about" className="nav-link"><span className="text-danger">O</span>ur Stoty</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="works" className="nav-link">What We Do</Link>
+                        <Link to="works" className="nav-link"><span className="text-danger">W</span>hat We Do</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/contact" className="nav-link">Reach Us</Link>
+                        <Link to="/contact" className="nav-link"><span className="text-danger">R</span>each Us</Link>
                     </li>
-                    <li className="nav-item">
-                        <button onClick={()=>setDarkMode(!darkMode)} className={`btn btn-sm ${darkMode ? "btn-light" : "btn-dark"}`}>
+                    <div className="nav-item ms-4">
+                        <button onClick={()=>setDarkMode(!darkMode)} className={`btn btn-lg ${darkMode ? "btn-light" : "btn-dark"}`}>
                             {darkMode ? (<>
                             <i class="fa-solid fa-sun"></i>
                             </>) :(<>
                             <i class="fa-regular fa-moon"></i>
                             </>)}
                         </button>
-                    </li>
+                    </div>
                 </ul>
             </div>
         </nav>
